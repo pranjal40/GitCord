3cf721ff281948995069c2469770c85de5da5c32
kritzz12 <kratika123yes@gmail.com>
2024-12-07 20:54:47 +0530

    Create average.c

---
+++ week-1/week1-kratika_sharma/average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+ float a,b,c,d,e;
+printf("enter the value of a\n");
+scanf("%f",&a);
+printf("enter the value of b\n");
+scanf("%f",&b);
+printf("enter the value of c\n");
+scanf("%f",&c);
+printf("enter the value of d\n");
+scanf("%f",&d);
+printf("enter the value of e\n");
+scanf("%f",&e);
+float avg=(a+b+c+d+e)/5;
+printf("the average of numbers is:%f\n",avg);
+return 0;
+}
